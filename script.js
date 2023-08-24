54c46ee6df7849b7011e231d35cf6c3e191efdf7
Emilia <empa0001@stud.kea.dk>
2023-08-24 10:52:13 +0200

    øvelse2b

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-const info = document.querySelector("h3");
-console.log(info);
-
-const text = document.querySelector("p");
-console.log(text);
-
 const image = document.querySelector("img");
 console.log(image);
 
 const footer = document.querySelector("footer");
 console.log(footer);
-
-const overskrift = document.querySelector("h1");
-console.log(overskrift);
 
 // pseudo selector
 // Query selector for the first article element
@@ -28,3 +19,38 @@ console.log(article3Element);
 // Query selector for the second article element
 const article4Element = document.querySelector("main > .grid1-1 > article:first-child + article");
 console.log(article4Element);
+
+// Query the header element
+const headerElement = document.querySelector("header");
+
+// Change the text content of the h1 element
+headerElement.querySelector("h1").textContent = "New Title";
+
+// for 1 article
+// Query the first article element
+const firstArticle = document.querySelector(".grid1-1 article:first-child");
+// Query the first p element within the first article
+const firstArticleParagraph = firstArticle.querySelector("p");
+
+// Change the text content of the first p element
+firstArticleParagraph.textContent = "New content for the first paragraph.";
+
+// for alle articles
+// Query all article elements
+const articleElements = document.querySelectorAll(".grid1-1 article");
+
+// Loop through each article and change the text content of the p element
+articleElements.forEach((article) => {
+  const paragraphElement = article.querySelector("p");
+  paragraphElement.textContent = "New content for the paragraph.";
+});
+
+// for alle articles overskrifter
+// Query all article elements
+const articleElementsh3 = document.querySelectorAll(".grid1-1 article");
+
+// Loop through each article and change the text content of the p element
+articleElementsh3.forEach((article) => {
+  const paragraphElement = article.querySelector("h3");
+  paragraphElement.textContent = "New h3.";
+});
